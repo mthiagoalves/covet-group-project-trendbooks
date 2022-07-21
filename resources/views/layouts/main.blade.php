a715b3e849c8199542a2c54a8cede3079a44d910
mthiagoalves <m.thiago.ales@live.com>
2022-07-21 18:31:03 +0100

    feat: impleme navbar, controllers, models and migrations

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>@yield('title')</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+    </head>
+
+        <body>
+
+            <nav class="navbar navbar-expand-lg" id="navbar">
+                <div class="container-fluid">
+                  <img src="/images/trendesignbook-logo.png" alt="logotipo" id="logo">
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                      <a class="nav-link active" aria-current="page" href="/">Book Store</a>
+                      <a class="nav-link" href="#">Wishlist</a>
+                    </div>
+                  </div>
+                </div>
+                @if (Route::has('login'))
+                    <div class="login-area">
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="text-log-in">Hi, {{auth()->user()->name}}</a>
+                        @else
+                            <a href="{{ route('login') }}" class="text-log-in">LOG-IN</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="text-log-in">REGISTER</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+                @guest
+                    @else
+                        <li class="nav-item">
+                            <div class="-menu -menu-end" aria-labelledby="navbar">
+                                <a class="-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                @endguest
+              </nav>
+
+              @yield('content')
+        </body>
+
+</html>
